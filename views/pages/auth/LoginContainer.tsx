fc3b9aaa29d9c59aa852c956a2fb751666a28e1d
cmrxnn <cmrxn2005@gmail.com>
2023-03-06 11:18:49 +0000

    chore(*): cleanup

--- views/pages/auth/LoginContainer.tsx
+++ views/pages/auth/LoginContainer.tsx
@@ -2,19 +2,14 @@ import Field from '../../elements/Field';
 import type { Component } from 'solid-js';
 import GuestLayout from '../../elements/GuestLayout';
 
-const LoginContainer: Component = () => {
-    const submit = (data: { name: string; password: string; }) => {
-
-    }
-    return (
-        <GuestLayout>
-            <p class="text-6xl text-center font-semibold">Log In to <span class="text-gray-700">Gradr</span>.</p>
-            <div class="my-10">
-                <Field name="email" placeholder="bill@microsoft.com" label="Username or Email" />
-                <Field name="password" type="password" label="Password" />
-            </div>
-        </GuestLayout>
-    );
-};
+const LoginContainer: Component = () => (
+    <GuestLayout>
+        <p class="text-6xl text-center font-semibold">Log In to <span class="text-gray-700">Gradr</span>.</p>
+        <div class="my-10">
+            <Field name="email" placeholder="bill@microsoft.com" label="Username or Email" />
+            <Field name="password" type="password" label="Password" />
+        </div>
+    </GuestLayout>
+);
 
 export default LoginContainer;
--- views/routers/AuthRouter.tsx
+++ views/routers/AuthRouter.tsx
@@ -1,16 +1,13 @@
+import NotFound from '../pages/NotFound';
 import type { Component } from 'solid-js';
 import { Route, Routes } from '@solidjs/router';
 import LoginContainer from '../pages/auth/LoginContainer';
-import NotFound from '../pages/NotFound';
 
-const AuthRouter: Component = () => {
-
-    return (
-        <Routes>
-            <Route path={'/login'} component={LoginContainer} />
-            <Route path={'/*'} component={NotFound} />
-        </Routes>
-    );
-};
+const AuthRouter: Component = () => (
+    <Routes>
+        <Route path={'/login'} component={LoginContainer} />
+        <Route path={'/*'} component={NotFound} />
+    </Routes>
+);
 
 export default AuthRouter;
